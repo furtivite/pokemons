8953ffa80e1ac84575001eca1296b3c438c26058
Egor Levchenko <antytoto@gmail.com>
2025-08-04 16:08:46 +0300

    feat(layout): display selected count in "In Comparison" header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { Logo } from "@components/Logo";
+import { selectSelectedPokemons, clearSelected } from '@features/compare/compareSlice';
+import { useAppDispatch, useAppSelector } from '../store';
 
 export const Header: React.FC = () => {
   const { pathname } = useLocation();
+  const selected = useAppSelector(selectSelectedPokemons);
+  const dispatch = useAppDispatch();
 
-  return (
-    <Navbar bg="light" expand="lg" className="sticky-top shadow-sm">
-      <Container>
-        <Navbar.Brand>
-          {pathname === '/' ? (
+return (
+  <Navbar bg="light" expand="lg" className="sticky-top shadow-sm">
+    <Container>
+      <Navbar.Brand>
+        {pathname === '/' ? (
+          <Logo />
+        ) : (
+          <Link to="/">
             <Logo />
-          ) : (
-            <Link to="/">
-              <Logo />
-            </Link>
-          )}
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+          </Link>
+        )}
+      </Navbar.Brand>
+      {selected.length > 0 && (
+        <Nav className="ms-auto d-flex align-items-center gap-2">
+          <Nav.Link
+            as={Link}
+            to="/compare"
+            className="p-0"
+          >
+            {selected.length} in Comparison 
+          </Nav.Link>
+          <Button
+            variant="outline-danger"
+            size="sm"
+            onClick={() => dispatch(clearSelected())}
+          >
+            Clear Comparison
+          </Button>
+        </Nav>
+      )}
+    </Container>
+  </Navbar>
   );
 };
