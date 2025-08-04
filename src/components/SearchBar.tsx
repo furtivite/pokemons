e62d8d471e8926434f099a5c26affa5ac66366f3
Egor Levchenko <antytoto@gmail.com>
2025-08-04 12:26:50 +0300

    refactor(ui): extract SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+interface SearchBarProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => (
+    <Form.Control
+        type="text"
+        placeholder="Search Pokémon"
+        value={value}
+        onChange={e => onChange(e.target.value)}
+    />
+);
