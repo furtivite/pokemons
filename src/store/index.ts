8e20bcab5f564bdb86a08a025433431143a67714
Egor Levchenko <antytoto@gmail.com>
2025-08-04 10:18:06 +0300

    feat(api): add RTK Query pokemonApi and integrate it into Redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,20 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { pokemonApi } from '../api/pokemonApi';
+import pokemonReducer from '../features/pokemon/pokemonSlice';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  pokemon: pokemonReducer,
+  [pokemonApi.reducerPath]: pokemonApi.reducer,
+});
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
