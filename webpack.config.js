00a9181cc0451ff625da9b83ab1ceea7434f750b
Egor Levchenko <antytoto@gmail.com>
2025-08-04 09:27:05 +0300

    chore: configure webpack bundler with HTML plugin and asset handling

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@features': path.resolve(__dirname, 'src/features/'),
+      '@api': path.resolve(__dirname, 'src/api/'),
+      '@utils': path.resolve(__dirname, 'src/utils/')
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|jpg|gif|svg)$/, type: 'asset/resource' }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    port: 3000
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
