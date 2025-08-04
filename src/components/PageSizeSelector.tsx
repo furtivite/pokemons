ad9a58bacbd562da7cd841022006d60e9823a2d9
Egor Levchenko <antytoto@gmail.com>
2025-08-04 12:28:31 +0300

    refactor(ui): extract PageSizeSelector component

---
+++ src/components/PageSizeSelector.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+interface PageSizeSelectorProps {
+    value: number;
+    onChange: (size: number) => void;
+}
+
+export const PageSizeSelector: React.FC<PageSizeSelectorProps> = ({ value, onChange }) => (
+    <Form.Select
+        value={value}
+        onChange={e => onChange(+e.target.value)}
+    >
+        {[10, 20, 50, 100].map(n => (
+            <option key={n} value={n}>
+                {n} per page
+            </option>
+        ))}
+    </Form.Select>
+);
