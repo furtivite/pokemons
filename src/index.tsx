0c3933b443a9b4db303aba234bee015fa13c9950
Egor Levchenko <50330458+furtivite@users.noreply.github.com>
2025-08-04 10:46:32 +0300

    chore(ui): add Bootstrap and React-Bootstrap (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find root element");
