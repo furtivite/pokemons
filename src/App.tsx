78e0b7a9f33f79968d76df8aac11b353c6cce3db
Egor Levchenko <antytoto@gmail.com>
2025-08-04 10:22:04 +0300

    feat(router): setup BrowserRouter in index.tsx and basic Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => (
-  <>App Works</>
+  <Routes>
+    <Route path="/" element={<>List</>} />
+    <Route path="/pokemon/:name" element={<>Details</>} />
+    <Route path="/compare" element={<>Compare</>} />
+  </Routes>
 );
 
 export default App;
