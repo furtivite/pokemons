4e3ca62b06ca04ea3bd62a7ca3ac4da4631aed3b
Egor Levchenko <antytoto@gmail.com>
2025-08-05 12:28:43 +0300

    refactor(components): organize each component into its own directory

---
+++ src/components/PokemonCardSkeleton/index.ts
@@ -0,0 +1,1 @@
+export { PokemonCardSkeleton } from './PokemonCardSkeleton';
--- src/components/PokemonDetailsSkeleton/PokemonDetailsSkeleton.tsx
+++ src/components/PokemonDetailsSkeleton/PokemonDetailsSkeleton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
