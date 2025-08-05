909ee4b53b29b9d205a04cd4885d86495b32549b
Egor Levchenko <antytoto@gmail.com>
2025-08-05 17:42:31 +0300

    feat(logo): import logo from src/assets for correct bundling

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 
-export const Logo: React.FC = () => <img src="/logo.svg" width={136} height={50} className="d-inline-block align-top" alt="Pokédex Logo" />;
+export const Logo: React.FC = () => <img src={logo} width={136} height={50} className="d-inline-block align-top" alt="Pokédex Logo" />;
